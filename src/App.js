b20c91f1c97f679959a3ea938aa4bb0d04a1e5da
Kim Blott <kimberlyanneblott@gmail.com>
2020-06-16 14:50:34 -0400

    Update bio and link styles

--- src/App.js
+++ src/App.js
@@ -9,7 +9,14 @@ const App = () => {
       <h1>
         Web Portfolio for <em>Kim Blott</em>
       </h1>
-      <p>Hello, my name is Kim, and welcome to my space on the web!</p>
+      <h2>Web Developer | Problem Solver | Coffee Drinker</h2>
+      <p>
+        I have been working as a web developer for about 4 years but have been
+        building websites since the late 1990s when I built my first website
+        using America Online&apos;s website builder. I love the thrill of
+        discovering how to use technology to solve real-world problems, build
+        innovative products, and solve challenging puzzles!
+      </p>
       <SocialLink />
     </div>
   );
