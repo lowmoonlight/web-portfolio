6cd33d1c56726879f1c91172075929ef53b4f531
Kim Blott <kimberlyanneblott@gmail.com>
2020-06-25 09:31:16 -0400

    Use destructuring for additional bio details

--- src/Details.js
+++ src/Details.js
@@ -10,12 +10,18 @@ const Details = () => {
       "reading, baking, drinking coffee, and walking when I could just as easily drive",
   };
 
+  const {
+    city: town,
+    state: region,
+    roommates: roomies,
+    hobbies: activities,
+  } = kimDetails;
+
   return (
     <div className="more-info">
       <p>
-        I currently live in {kimDetails.city}, {kimDetails.state} with a variety
-        of {kimDetails.roommates}. In my free time, I gravitate toward{" "}
-        {kimDetails.hobbies}.
+        I currently live in {town}, {region} with a variety of {roomies}. In my
+        free time, I gravitate toward spending my time on {activities}.
       </p>
       <p>
         <Link to="/">Back to the home page</Link>
