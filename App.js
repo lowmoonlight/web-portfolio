c2693b5dc938e0d798cf21156d5b1c596ffcfc00
Kimberly Blott <32112405+lowmoonlight@users.noreply.github.com>
2024-10-07 17:36:33 -0400

    Update personal and professional details

---
+++ App.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { render } from "react-dom";
+import { Router } from "@reach/router";
+import "./App.scss";
+import SocialLink from "./SocialLink";
+import Intro from "./Intro";
+import Details from "./Details";
+import Heart from "./Heart";
+import kimImage from "./images/kim.jpg";
+
+const App = () => {
+  return (
+    <div id="wrapper">
+      <h1>
+        Web Portfolio for <em>Kim Blott</em>
+      </h1>
+      <img
+        src={kimImage}
+        className="kim-image"
+        alt="Kim Blott smiling into camera"
+      />
+      <h2>Web Developer | Problem Solver | Coffee Drinker</h2>
+      <Router>
+        <Intro path="/" />
+        <Details path="/details" />
+      </Router>
+      <SocialLink />
+      <footer>
+        <hr />
+        <Heart />
+      </footer>
+    </div>
+  );
+};
+
+render(<App />, document.getElementById("root"));
---
+++ Intro.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Link } from "@reach/router";
+
+const Intro = () => {
+  return (
+    <div className="introduction">
+      <p>
+        I have been working as a web developer professionally since 2016 but have been
+        building websites since the late 1990s when I built my first website
+        using America Online&apos;s website builder. I have mostly worked with
+        HTML, CSS/Sass, jQuery, and PHP but am currently studying vanilla
+        JavaScript and React to get up to speed with those languages. There is
+        nothing better than the thrill of getting things built using code!
+      </p>
+      <p>
+        <Link to="/details">Learn more about Kim</Link>
+      </p>
+    </div>
+  );
+};
+
+export default Intro;
