fe92fbb63520593fd25daa9aff196ced93074710
Kimberly Blott <32112405+lowmoonlight@users.noreply.github.com>
2024-10-07 18:01:34 -0400

    Update personal and professional details

--- src/Intro.js
+++ src/Intro.js
@@ -5,7 +5,7 @@ const Intro = () => {
   return (
     <div className="introduction">
       <p>
-        I have been working as a web developer for about 4 years but have been
+        I have been working as a web developer professionally since 2016 but have been
         building websites since the late 1990s when I built my first website
         using America Online&apos;s website builder. I have mostly worked with
         HTML, CSS/Sass, jQuery, and PHP but am currently studying vanilla
